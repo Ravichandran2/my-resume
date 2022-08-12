7c284282983689e94ea516db4fdad70d188c32d8
Ravichandran R <r2d2.chandran@gmail.com>
2022-08-12 22:28:33 +0530

    Azure func API local

--- frontend/main.js
+++ frontend/main.js
@@ -2,15 +2,15 @@ window.addEventListener('DOMContentLoaded', (event) => {
     getVisitCount();
 })
 
-const functionApi = '';
+const functionApi = 'http://localhost:7071/api/GetResumeCounter';
 
 const getVisitCount = () => {
     let count = 50;
     fetch(functionApi).then(response => {
-        return response.json;
+        return response.json();
     }).then(response => {
         console.log("API Hit");
-        document.getElementById("count").innerText = response.count;
+        document.getElementById("counter").innerText = response.count;
     }).catch(error => {
         console.log(error());
     });
